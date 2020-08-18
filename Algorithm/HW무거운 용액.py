9b604f241374f421913410e9593c3c7906861f2e
cmlee119 <Your email address>
2020-08-18 12:37:40 +0900

    tt

--- Algorithm/HW무거운 용액.py
+++ Algorithm/HW무거운 용액.py
@@ -3,7 +3,7 @@ sys.stdin = open("./Algorithm/HW무거운 용액.txt", "r")
 
 T = int(input())
 for test_case in range(1, T + 1):
-    N, C = map(int, input().split())
+    N, C = tuple(int(n) for n in input().split())
 
     listGramVolume = [[int(E) for E in input().split()] for _ in range(N)]
     listGramVolume.sort(key=lambda t:t[0]/t[1], reverse=True)
@@ -11,8 +11,13 @@ for test_case in range(1, T + 1):
     V = C
     resultMass = 0
     for gramVolume in listGramVolume:
-        subVolume = min(V, gramVolume[1])
-        resultMass += subVolume * gramVolume[0] / gramVolume[1]
+        if gramVolume[1] > V:
+            subVolume = V
+            resultMass += subVolume * gramVolume[0] / gramVolume[1]
+        else:
+            subVolume = gramVolume[1]
+            resultMass += gramVolume[0]
+
         V -= subVolume
         if V == 0:
             break
